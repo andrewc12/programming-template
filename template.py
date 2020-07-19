8ee8820e307564660e173f1229590b20d6b6b2e3
Andrew Innes <19179944@student.curtin.edu.au>
2020-07-19 14:28:58 +0800

    clean up

--- template.py
+++ template.py
@@ -5,21 +5,17 @@ from jinja2 import Environment, BaseLoader
 import argparse
 import json
 
-
-
-def render_template(template_file, context):
-    with template_file as f:
-        TEMPLATE_ENVIRONMENT = Environment(
-        autoescape=False,
-        loader=BaseLoader(),
-        trim_blocks=False,
-        extensions=['jinja2.ext.do']).from_string(f.read())
+def render_template(templatefile, context):
+    TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=BaseLoader(),
+    trim_blocks=False,
+    extensions=['jinja2.ext.do']).from_string(templatefile.read())
     return TEMPLATE_ENVIRONMENT.render(context)
 
-def render_template_to_file(template_filename, output_file, context):
-    with output_file as f:
-        output = render_template(template_filename, context)
-        f.write(output)
+def render_template_to_file(templatefile, outputfile, context):
+    output = render_template(templatefile, context)
+    outputfile.write(output)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -33,10 +29,10 @@ def main():
     with args.contextfile as contextfile:
         context = json.load(contextfile)
         
-    print(context)
-    #print(args)
-    
-    render_template_to_file(args.templatefile, args.outfile, context)
+    #print(context)
+
+    with args.templatefile as templatefile, args.outfile as outputfile:
+        render_template_to_file(templatefile, outputfile, context)
 
 
 #############################################################################
